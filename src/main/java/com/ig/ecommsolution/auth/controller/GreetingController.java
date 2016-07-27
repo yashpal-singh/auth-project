955d20ceb620f5a008cfaff23ee7b52c613b4701
Yashpal Singh <yashpal.singh@infogain.com>
2016-07-27 10:38:05 +0530

    Secure method access with roles.

--- src/main/java/com/ig/ecommsolution/auth/controller/GreetingController.java
+++ src/main/java/com/ig/ecommsolution/auth/controller/GreetingController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class GreetingController {
 	@Autowired
 	private UserRepository repository;
 
+//	@PreAuthorize("hasRole('Tester')")
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<User> greeting() {
 		List<User> users = repository.findAll();
